61049d9f42123591d78339ce2d6ef562780e8cd7
java1777 <108736263+java1777@users.noreply.github.com>
2025-04-10 09:15:52 +0500

    lesson-4

---
+++ main.js
@@ -0,0 +1,29 @@
+// const person = {
+//     name: "usmon",
+//     age: 33,
+//     gender: "MALE",
+//     hobbies: [],
+//     skills: [],
+//  };
+
+//  alert(person.name);
+
+//  const firstName = prompt("Ismingizni kiriting");
+//  const lastName = prompt("Familiyangizni kiriting");
+//  const age = parseInt(prompt("Yoshingiz?"));
+//  console.log(firstName);
+//  console.log(lastName);
+//  console.log(typeof age);
+//  console.log(age);
+
+
+//  if (age>20) {
+//     console.log()
+//  }
+
+
+const grade = parseInt(prompt("Bahoni kiriting!"));
+
+if (grade <= 0) {
+    console.log("Siz notog'ri son kiritdingiz");
+}   else if (grade >= 1 );
